11b357872f469cf9d0180aea3e0f23638764aca2
otsuka.amane <otsuka.amane@gmail.com>
2020-05-22 08:38:51 +0900

    Initial Commit

---
+++ PropertyWrapperTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PropertyWrapperTest
+//
+//  Created by 大塚周 on 2020/05/22.
+//  Copyright © 2020 大塚周. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
