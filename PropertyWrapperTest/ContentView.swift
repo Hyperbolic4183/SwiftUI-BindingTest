e7dda9963281c70b53d223084dcd508707d3615b
otsuka.amane <otsuka.amane@gmail.com>
2020-05-22 09:01:48 +0900

    ContentViewのcountには@State属性が付与されている。一つの変数を複数のオブジェクトで共有したい時に用いる。
    MyStepperには@Binding属性が付与されている。実際の値は他の構造体で定義され、共有されている。
    抽象的な数字を引数とするイメージ

--- PropertyWrapperTest/ContentView.swift
+++ PropertyWrapperTest/ContentView.swift
@@ -9,8 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("ContentView\(count)")
+            Button(action: {
+                self.count = self.count + 10
+            }) {
+                Text("count + 10")
+            }
+            MyStepper(value: $count)
+        }
+    }
+}
+
+struct MyStepper: View {
+    @Binding var value: Int
+    
+    var body: some View {
+        HStack {
+            Text("MyStepper: \(value)")
+            Button(action: {
+                self.value = self.value - 1
+            }) {
+                Image(systemName: "minus")
+                    .foregroundColor(.white)
+                .padding()
+                    .background(Color.red)
+                .cornerRadius(5)
+                
+            }
+            Button(action: {
+                self.value = self.value +  1
+            }){
+                Image(systemName: "plus")
+                    .foregroundColor(.white)
+                .padding()
+                    .background(Color.blue)
+                .cornerRadius(5)
+            }
+        }
+    .padding()
     }
 }
 
